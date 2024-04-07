879b954ea90714fe383f4b046b396ebce4e18b7b
Akhil Singh <akhilkumarsingh428@gmail.com>
2024-04-07 10:50:18 +0530

    router made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <>hii</>;
+  return (
+    <>
+      <Routes>
+        <Route to="/" element={<Home />} />
+        <Route to="/sign-in" element={<SignIn />} />
+        <Route to="/sign-out" element={<SignOut />} />
+        <Route to="/about" element={<About />} />
+        <Route to="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
